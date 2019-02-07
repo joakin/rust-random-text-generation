8b92c032d63928e2532753b98096fbbb5c7b7795
joakin <joaquin@chimeces.com>
2019-02-07 20:56:30 +0100

    Cleanup: Rename and split tests appropriately

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,30 @@ use rand::Rng;
 use std::collections::HashMap;
 
 const START: &str = "START";
-
 const STOP: &str = "STOP";
 
 #[derive(Debug)]
-struct Distribution {
+struct WordDistribution {
     total: u32,
-    amounts: HashMap<String, u32>,
+    words: HashMap<String, u32>,
 }
 
-impl Distribution {
-    fn new() -> Distribution {
+impl WordDistribution {
+    fn new() -> WordDistribution {
         let total = 0;
-        let amounts = HashMap::new();
-        Distribution { total, amounts }
+        let words = HashMap::new();
+        WordDistribution { total, words }
     }
 
     fn add(&mut self, word: String) {
         self.total += 1;
-        let count = self.amounts.entry(word).or_default();
+        let count = self.words.entry(word).or_default();
         *count += 1;
     }
 
     fn get_random(&self) -> Option<&str> {
         let mut n = rand::thread_rng().gen_range(0, self.total);
-        for (word, amount) in self.amounts.iter() {
+        for (word, amount) in self.words.iter() {
             if n <= *amount {
                 return Some(&word);
             }
@@ -36,51 +35,69 @@ impl Distribution {
     }
 }
 
-#[derive(Debug)]
-struct PTable {
-    prefix_length: usize,
-    table: HashMap<Vec<String>, Distribution>,
+#[cfg(test)]
+mod word_distribution_tests {
+    use super::*;
+
+    #[test]
+    fn new_distribution() {
+        let words = vec!["hi", "ho", "hi"];
+        let mut distribution = WordDistribution::new();
+        for word in words {
+            distribution.add(word.to_string());
+        }
+        assert_eq!(distribution.total, 3, "Total should be 3");
+        assert_eq!(
+            *distribution.words.entry("hi".to_string()).or_default(),
+            2,
+            "hi should have frequency 2"
+        );
+    }
 }
-impl PTable {
-    fn new(prefix_length: u32) -> PTable {
-        PTable {
+
+#[derive(Debug)]
+struct PrefixTable {
+    prefix_length: usize,
+    table: HashMap<Vec<String>, WordDistribution>,
+}
+
+impl PrefixTable {
+    fn new(prefix_length: u32) -> PrefixTable {
+        PrefixTable {
             prefix_length: prefix_length as usize,
             table: HashMap::new(),
         }
     }
 
     fn add_sentence(&mut self, sentence: Vec<String>) -> &mut Self {
-        let mut prefix: Vec<String> = Vec::with_capacity(self.prefix_length);
+        let mut prefix = Prefix::new(self.prefix_length);
         prefix.push(START.to_string());
 
         for word in sentence {
             let distribution = self
                 .table
-                .entry(prefix.clone())
-                .or_insert(Distribution::new());
+                .entry(prefix.words.clone())
+                .or_insert(WordDistribution::new());
             distribution.add(word.clone());
 
-            if prefix.len() == self.prefix_length {
-                prefix.remove(0);
-            }
             prefix.push(word);
         }
 
         let distribution = self
             .table
-            .entry(prefix.clone())
-            .or_insert(Distribution::new());
+            .entry(prefix.words.clone())
+            .or_insert(WordDistribution::new());
         distribution.add(STOP.to_string());
 
         self
     }
 
     fn walk(&self) -> Vec<String> {
-        let mut prefix: Vec<String> = Vec::with_capacity(self.prefix_length);
+        let mut prefix = Prefix::new(self.prefix_length);
         prefix.push(START.to_string());
         let mut result = Vec::new();
 
-        while let Some(distribution) = self.table.get(&prefix) {
+        while let Some(distribution) = self.table.get(&prefix.words) {
             match distribution.get_random() {
                 Some(word) => {
                     if word == STOP {
@@ -89,9 +106,6 @@ impl PTable {
 
                     result.push(word.to_string());
 
-                    if prefix.len() == self.prefix_length {
-                        prefix.remove(0);
-                    }
                     prefix.push(word.to_string());
                 }
                 None => return result,
@@ -99,6 +113,85 @@ impl PTable {
         }
 
         result
+    }
+}
+
+#[cfg(test)]
+mod prefix_table_tests {
+    use super::*;
+
+    #[test]
+    fn prefix_table() {
+        let text = "\
+I'm nobody! Who are you?
+Are you nobody, too?
+Then there's a pair of us - don't tell!
+They'd banish us, you know.
+
+How dreary to be somebody!
+How public, like a frog
+To tell your name the livelong day
+To an admiring bog!
+
+Are you nobody, as well?";
+        let mut prefix_table = PrefixTable::new(3);
+        for sentence in sentences::from_text(text) {
+            prefix_table.add_sentence(sentence);
+        }
+        assert_eq!(prefix_table.prefix_length, 3);
+        assert_eq!(prefix_table.table.len(), 57);
+        assert_eq!(
+            *prefix_table
+                .table
+                .entry(vec![
+                    "Are".to_string(),
+                    "you".to_string(),
+                    "nobody".to_string()
+                ])
+                .or_insert(WordDistribution::new())
+                .words
+                .entry(",".to_string())
+                .or_default(),
+            2
+        );
+    }
+}
+
+struct Prefix {
+    length: usize,
+    words: Vec<String>,
+}
+
+impl Prefix {
+    fn new(length: usize) -> Prefix {
+        Prefix {
+            length,
+            words: Vec::with_capacity(length),
+        }
+    }
+
+    fn push(&mut self, word: String) -> &mut Prefix {
+        if self.words.len() == self.length {
+            self.words.remove(0);
+        }
+        self.words.push(word.to_string());
+        self
+    }
+}
+
+#[cfg(test)]
+mod prefix_tests {
+    use super::*;
+
+    #[test]
+    fn prefix_test() {
+        let mut prefix = Prefix::new(2);
+        prefix.push("hi".to_string());
+        prefix.push("ho".to_string());
+        assert_eq!(prefix.words, vec!["hi".to_string(), "ho".to_string()]);
+
+        prefix.push("lets go".to_string());
+        assert_eq!(prefix.words, vec!["ho".to_string(), "lets go".to_string()]);
     }
 }
 
@@ -154,88 +247,64 @@ mod sentences {
             sentence.clear();
         }
     }
+
+    #[cfg(test)]
+    mod sentence_tests {
+        use super::*;
+
+        #[test]
+        fn parsing_sentences() {
+            let tests = vec![
+                (
+                    "Hi, how are you?",
+                    vec![vec!["Hi", ",", "how", "are", "you", "?"]],
+                ),
+                (
+                    "Hi, how are you? I am fine thanks!",
+                    vec![
+                        vec!["Hi", ",", "how", "are", "you", "?"],
+                        vec!["I", "am", "fine", "thanks", "!"],
+                    ],
+                ),
+                (
+                    "Hi, how are you? Fine",
+                    vec![vec!["Hi", ",", "how", "are", "you", "?"], vec!["Fine"]],
+                ),
+            ];
+            for (t1, s1) in tests {
+                assert_eq!(from_text(t1), s1);
+            }
+        }
+    }
+
 }
 
 pub struct SentenceGenerator {
-    ptable: PTable,
+    prefix_table: PrefixTable,
 }
 
 impl SentenceGenerator {
     pub fn new(prefix_length: u32) -> SentenceGenerator {
         SentenceGenerator {
-            ptable: PTable::new(prefix_length),
+            prefix_table: PrefixTable::new(prefix_length),
         }
     }
 
     pub fn add_text(&mut self, text: &str) {
         let sentences = sentences::from_text(text);
         for sentence in sentences {
-            self.ptable.add_sentence(sentence);
+            self.prefix_table.add_sentence(sentence);
         }
     }
 
     pub fn get_random_sentence(&self) -> String {
-        self.ptable.walk().join(" ")
+        self.prefix_table.walk().join(" ")
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod sentence_generator_tests {
     use super::*;
-
-    #[test]
-    fn new_distribution() {
-        let words = vec!["hi", "ho", "hi"];
-        let mut distribution = Distribution::new();
-        for word in words {
-            distribution.add(word.to_string());
-        }
-        dbg!(distribution);
-    }
-
-    #[test]
-    fn parsing_sentences() {
-        let tests = vec![
-            (
-                "Hi, how are you?",
-                vec![vec!["Hi", ",", "how", "are", "you", "?"]],
-            ),
-            (
-                "Hi, how are you? I am fine thanks!",
-                vec![
-                    vec!["Hi", ",", "how", "are", "you", "?"],
-                    vec!["I", "am", "fine", "thanks", "!"],
-                ],
-            ),
-            (
-                "Hi, how are you? Fine",
-                vec![vec!["Hi", ",", "how", "are", "you", "?"], vec!["Fine"]],
-            ),
-        ];
-        for (t1, s1) in tests {
-            assert_eq!(sentences::from_text(t1), s1);
-        }
-    }
-
-    #[test]
-    fn ptable() {
-        let text = "\
-I'm nobody! Who are you?
-Are you nobody, too?
-Then there's a pair of us - don't tell!
-They'd banish us, you know.
-
-How dreary to be somebody!
-How public, like a frog
-To tell your name the livelong day
-To an admiring bog!";
-        let text2 = "Hi how are you. Hey, how are thee. Hi, how are they? No, how are thee!";
-        let mut ptable = PTable::new(2);
-        for sentence in sentences::from_text(text2) {
-            ptable.add_sentence(sentence);
-        }
-        dbg!(ptable);
-    }
 
     #[test]
     fn sentence_generator() {
@@ -251,6 +320,9 @@ To tell your name the livelong day
 To an admiring bog!";
         let mut sentence_generator = SentenceGenerator::new(2);
         sentence_generator.add_text(text);
-        dbg!(sentence_generator.get_random_sentence());
+        assert!(
+            sentence_generator.get_random_sentence().len() > 0,
+            "Should return a sentence with at least one word"
+        );
     }
 }
