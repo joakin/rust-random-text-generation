612ed61cd955fe67b2f49d06673274d4328bfc7a
joakin <joaquin@chimeces.com>
2019-02-17 14:06:39 +0100

    Add benchmark for sentence creation

---
+++ benches/sentence_generator.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use rust_random_text_generation::SentenceGenerator;
+use std::fs;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let alice = fs::read_to_string("./books/alice-in-wonderland.txt").unwrap();
+    c.bench_function("make sentence generator", move |b| {
+        let alice_part = &alice[0..10000]; // It is ~150000 chars
+
+        b.iter(|| {
+            let mut sg = SentenceGenerator::new(5);
+            sg.add_text(alice_part);
+            sg
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
